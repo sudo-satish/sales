9bdf6f9211fcc60977ff191fd83fde57758fbd71
teamsatish <32234575+teamsatish@users.noreply.github.com>
2018-06-02 23:11:02 +0530

    Home page link added.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,9 +83,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="/sys/global-value">Global Value</a>
+                    <a href="/rpt/define-report">Define Report</a>
+                    <a href="/rpt/download-report">Download Report</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
